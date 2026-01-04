8393e26ad2a88763e8ccc887120267044657fe7a
betapahatid-oss <betapahatid@gmail.com>
2026-01-04 23:43:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-document.getElementById("formKontak").addEventListener("submit", function(e) {
+const nomorWA = "628xxxxxxxxxx"; // GANTI NOMOR WA
+
+function beliProduk(namaProduk) {
+    let url = "https://wa.me/" + nomorWA +
+              "?text=Halo,%20saya%20ingin%20membeli:%20" + namaProduk;
+    window.open(url, "_blank");
+}
+
+document.getElementById("formServis").addEventListener("submit", function(e){
     e.preventDefault();
-
     let nama = document.getElementById("nama").value;
-    let pesan = document.getElementById("pesan").value;
+    let jenis = document.getElementById("jenis").value;
 
-    let nomorWA = "628xxxxxxxxxx"; // GANTI nomor WhatsApp
     let url = "https://wa.me/" + nomorWA +
               "?text=Halo,%20saya%20" + nama +
-              "%20ingin%20servis:%20" + pesan;
-
+              "%20ingin%20servis:%20" + jenis;
     window.open(url, "_blank");
 });
